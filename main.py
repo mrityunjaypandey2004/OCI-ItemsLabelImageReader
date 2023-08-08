423615bea179370f3cfc31094eafab30a6946568
mrityunjay <mrityunjay.pandey.2004@gmail.com>
2023-08-08 00:30:30 -0500

    main py commit

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# This is a sample program for OCR using Paddle OCR lib, reading text in images.
+# This program uses Paddle OCR library to perform OCR functions, we use the models to extract text from images passed.
+from paddleocr import PaddleOCR, draw_ocr   # OCR library dependencies
+from matplotlib import pyplot as plt  # for plotting images
+import cv2  # open cv
+import os  # for opening directories
+
+ocr_model = PaddleOCR(use_angle_cls=True, lang='en')  # instantiate the model
+img_path = os.path.join('.', 'SampleImage.jpeg')
+print(img_path)
+result = ocr_model.ocr(img_path, cls=True)
+print('-------')
+print(result)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
